9721739b2a4c260a1e45131ebdb2912b72183c30
seasons986 <annalin12345@gmail.com>
2023-11-09 01:36:43 -0500

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+  const container = document.getElementById("container");
+  const portal = document.getElementById("portal");
+  const portalID = portal.dataset.portalid;
+  let rooms = ["日", "月", "山", "水", "火"];
+  const room = portalID ? portalID : rooms[getRandomInt(0,rooms.length)];
+  const chWidth = 70;
+  const chHeight = 90;
+
+  function initialize() {
+    for (let i = 0; i < window.innerWidth; i+=chWidth) {
+      for (let j = 0; j < window.innerHeight; j+=chHeight) {
+        container.innerHTML += `&nbsp;<img src="${room}.svg"/>&nbsp;`;
+      }
+    }
+    rooms = rooms.filter((el) => el!=room)
+    portal.innerText = rooms[getRandomInt(0,rooms.length)];
+    portal.onclick = (e) => {
+      e.preventDefault();
+      const text = `<!doctypehtml><title>text adventure</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><link href=https://iguannalin.github.io/pictograph/index.css rel=stylesheet><script src=https://iguannalin.github.io/pictograph/index.js></script><div id=container></div><div id=overlay><a data-portalid=${portal.innerText} id=portal></a></div>`;
+      const blob = new Blob([text], {type: "text/html"});
+      const blobUrl = URL.createObjectURL(blob);
+      window.open(blobUrl, '_self');
+      window.URL.revokeObjectURL(blobUrl);
+    }
+  }
+  initialize();
+});
